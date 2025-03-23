81e6cde3aef8ef6710fafe77594fd57462467a25
Amoghajeevi <154074823+Amoghajeevi@users.noreply.github.com>
2025-03-23 19:54:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello, Dockerjenkinsproject')
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from Docker Jenkins Pipeline!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
